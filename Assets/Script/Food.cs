64a223bcd910ea4090613d4dedc7cd93a3fedd01
AntoanGeorgiev <antoangeorgiev@yahoo.com>
2018-01-04 23:50:50 +0200

    First version

---
+++ Assets/Script/Food.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour {
+
+	public bool Active{set;get;}
+	public SpriteRenderer sRenderer;
+	private const float gravity = 2.0f;
+	private float speed; 
+	private float verticalVelocity;
+	private bool isSliced = false;
+	public Sprite[] sprites;
+	private int spriteIndex;
+	private float lastSpriteUpdate;
+	private float spriteUpdateDelta = 0.125f;
+
+
+
+	public void FoodLauncher(float verticalVelocity, float xSpeed, float xStart)
+	{
+		Active = true;
+		speed = xSpeed;
+		this.verticalVelocity = verticalVelocity;
+		transform.position = new Vector3 (xStart, 0, 0);
+		isSliced = false;
+		spriteIndex = 0;
+		sRenderer.sprite = sprites [spriteIndex];
+	}
+
+	private void Update()
+	{
+		if (!Active)
+			return;
+		verticalVelocity -= gravity * Time.deltaTime;
+		transform.position += new Vector3 (speed, verticalVelocity, 0) * Time.deltaTime;
+	
+		if (isSliced) {
+			if (spriteIndex != sprites.Length-1 &&Time.time - lastSpriteUpdate > spriteUpdateDelta) {
+				lastSpriteUpdate = Time.time;
+				spriteIndex++;
+				sRenderer.sprite = sprites [spriteIndex];
+			}
+		}
+		if (transform.position.y < -1) {
+			Active = false;
+			if (!isSliced)
+				Manager.Instance.LoseLP ();
+		}
+	}
+
+	public void Slice()
+	{
+		if (isSliced) 
+			return;
+		if (verticalVelocity < 0.5f) 
+			verticalVelocity = 0.5f;
+		speed=speed*0.5f;
+		isSliced = true;
+
+		Manager.Instance.IncrementScore (1);
+	}
+
+
+
+}
---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour {
+	private List<Food> food = new List<Food>();
+	public GameObject foodPrefab;
+	public GameObject pauseMenu;
+	private Collider2D[] foodCols;
+	private float deltaSpawn=1.0f;
+	private float lastSpawn;
+	private const float Sliceforce = 300.0f; 
+	private int scoreCount;
+	private int highScore;
+	private Vector3 LastMousePosition;
+	public Transform trail;
+	private int lifePoint;
+	public Text scoreText;
+	public Text highscoreText;
+	public Image[] points;
+	public static Manager Instance;
+	private bool isPaused = false;
+	public GameObject deathMenu;
+	public GameObject Sound;
+
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+		
+	private void Start()
+	{
+		foodCols = new Collider2D[0];
+		NewGame ();
+	}
+
+	public void NewGame()
+	{
+		scoreCount = 0;
+		lifePoint = 3;
+		pauseMenu.SetActive (false);
+		scoreText.text = scoreCount.ToString ();
+		highScore = PlayerPrefs.GetInt ("Score");
+		highscoreText.text = "Best: " + highScore.ToString (); 
+		Time.timeScale = 1;
+		isPaused = false;
+		deathMenu.SetActive (false);
+		foreach (Image i in points)
+			i.enabled = true;
+		foreach (Food f in food) {
+			Destroy (f.gameObject);
+			food.Clear ();
+		}
+			
+	
+	}	
+
+
+	private void Update()
+	{
+		if (isPaused)
+			return;
+
+		if (Time.time - lastSpawn > deltaSpawn) {
+			Food f = getFood ();
+			float randomX = Random.Range (-1.65f, 1.65f);
+
+			f.FoodLauncher (Random.Range (1.85f, 2.75f), randomX, -randomX);
+			lastSpawn = Time.time;
+		}
+		if (Input.GetMouseButton (0)) {			
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			pos.z = -1;
+			trail.position = pos;
+
+			Collider2D[] thisFramesFood = Physics2D.OverlapPointAll (new Vector2 (pos.x, pos.y), LayerMask.GetMask ("Banana"));
+			if ((Input.mousePosition - LastMousePosition).sqrMagnitude > Sliceforce) { 
+				foreach (Collider2D c2 in thisFramesFood) {
+					for (int i = 0; i < foodCols.Length; i++) {
+						if (c2 == foodCols [i]) {
+							c2.GetComponent<Food> ().Slice ();
+							Sound.GetComponent<AudioSource>().Play ();
+						}
+					}
+				}
+			}
+			LastMousePosition = Input.mousePosition;
+			foodCols = thisFramesFood;
+		}
+	}
+
+	private Food getFood() // Seeking food
+	{
+		Food f = food.Find (x => !x.Active);
+		if (f == null) {
+			f = Instantiate (foodPrefab).GetComponent<Food> ();
+			food.Add (f);
+		}
+		return f;
+	}
+
+	public void IncrementScore (int scoreAmount)
+	{
+		scoreCount += scoreAmount;
+		scoreText.text = scoreCount.ToString ();
+
+		if (scoreCount > highScore) {
+			highScore = scoreCount;
+			highscoreText.text = "BEST:" + highScore.ToString (); 
+			PlayerPrefs.SetInt ("Score", highScore);
+		}
+		}
+
+	public void LoseLP()
+	{
+		if (lifePoint == 0) {
+			return;
+		}
+		lifePoint--;
+		points[lifePoint].enabled = false;
+		if (lifePoint == 0)
+			Death ();
+	}
+
+	public void Death()
+	{
+		isPaused = true;
+		deathMenu.SetActive (true);
+	}
+	 
+	public void PauseGame()
+	{
+		pauseMenu.SetActive (!pauseMenu.activeSelf);
+		isPaused = pauseMenu.activeSelf;
+		Time.timeScale = (Time.timeScale == 0) ? 1: 0 ;
+	}
+
+	public void ToMain()
+	{
+		SceneManager.LoadScene ("main");
+	}
+
+
+
+}
